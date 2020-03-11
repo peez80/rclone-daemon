b365b054865353182c299cf0268c59bccb52e521
peez80 <peez@stiffi.de>
2020-03-11 19:49:32 +0100

    Change invocation of rclone because filters didn't work before

--- rclone-daemon.sh
+++ rclone-daemon.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 # env: RCLONE_COMMAND
 
+THE_COMMAND="rclone $RCLONE_COMMAND"
+
 while true
 do
-    echo "------------ EXECUTE RCLONE: rclone $RCLONE_COMMAND ---------------"
-    rclone $RCLONE_COMMAND
+    echo "------------ EXECUTE RCLONE: $THE_COMMAND ---------------"
+    echo "wait time after: $RCLONE_SLEEP"
+    echo ""
+    sh -c "$THE_COMMAND"
     echo "SLEEPING $RCLONE_SLEEP"
     sleep $RCLONE_SLEEP
 done
